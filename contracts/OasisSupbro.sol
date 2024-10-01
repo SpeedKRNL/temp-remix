35f717a253157275b9e0adebedcb34e7d8ae061b
UPALAJ SVAVASU <speed@krnl.xyz>
2024-10-02 02:17:21 +0700

    zzz

---
+++ contracts/OasisSupbro.sol
@@ -0,0 +1,10 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.20;
+
+import "@oasisprotocol/sapphire-contracts/contracts/Sapphire.sol";
+
+contract OasisSupbro {
+  function sayHelloWorld() public pure returns (string memory) {
+        return "Hello World from Oasis Sup bro lol";
+    }
+}
---
+++ contracts/Supbro.sol
@@ -0,0 +1,7 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.20;
+contract Supbro {
+    function sayHelloWorld() public pure returns (string memory) {
+        return "Hello World sup bro lol";
+    }
+}
